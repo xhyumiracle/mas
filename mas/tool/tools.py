189da92a282d8d2c937a1850f5b70ff7c75b744e
hengzzzhou <zhouheng@bupt.edu.cn>
2025-03-25 20:15:28 +0800

    chore: add some tool

--- mas/tool/tools.py
+++ mas/tool/tools.py
@@ -1,9 +1,21 @@
 from agno.tools.duckduckgo import DuckDuckGoTools
-from agno.tools.googlesearch import GoogleSearchTools
 from agno.tools.wikipedia import WikipediaTools
+from agno.tools.browserbase import BrowserbaseTools
+from agno.tools.youtube import YouTubeTools
+from agno.tools.website import WebsiteTools 
+from agno.tools.csv_toolkit import CsvTools
+from agno.tools.file import FileTools
+from agno.tools.calculator import CalculatorTools
+from agno.tools.arxiv import ArxivTools
 
 TOOLS = {
-    'duckduckgo': DuckDuckGoTools(),
-    'googlesearch': GoogleSearchTools(),
+    'duckduckgo': DuckDuckGoTools(), 
     'wikipedia': WikipediaTools(),
+    'browser': BrowserbaseTools(),
+    'youtube': YouTubeTools(),
+    'website': WebsiteTools(), 
+    'csv': CsvTools(),
+    'file': FileTools(),
+    'calculator': CalculatorTools(),
+    'arxiv': ArxivTools(),
 }
