f1b887c9bf342872524e66a9a43864271ddc7c7a
xhyumiracle <xhyumiracle@gmail.com>
2025-03-30 01:14:53 +0800

    chore(test): fix test_tool when pytest all

--- tests/test_tool.py
+++ tests/test_tool.py
@@ -4,6 +4,6 @@ from mas.graph.types import NodeAttr
 def test_to_tool():
     from mas.agent import MockAgent
 
-    ToolPool().initialize() # load builtin tools
+    ToolPool().get_global() # load builtin tools
     agent = MockAgent(id=1, node_attr=NodeAttr(name="test", prompt="test", profile="test", model="gpt-4o", tools=["duckduckgo"], input_formats=["text"], output_formats=["text"]))
     print(agent.to_tools(["duckduckgo"]))
